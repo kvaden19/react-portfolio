3f030c60fdddc80163fb9e206891f60c1f0f5668
Kelly Vaden <klvaden@gmail.com>
2021-06-26 19:10:59 -0400

    Wrote Footer component to use in all pages. Populated Contact.js page with placeholder content.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+function Footer() {
+    return (
+        <div id='footer'>
+            <h6 class='text-center'>Copyright 2021 Kelly L Vaden</h6>
+        </div>
+    );
+}
+
+export default Footer;
